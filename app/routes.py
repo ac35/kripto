b55feb4a900342d66b3ac9ad22ecea093f9384f8
alvin <alvinchandra783@gmail.com>
2018-07-20 20:46:41 +0700

    Tambah halaman login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -7,3 +8,14 @@ from app import app
 def index():
     user = {'username': 'alvin'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        #  infrastruktur utk log users for real belum tersedia
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data)) #  sementara aja pake cara ini
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
