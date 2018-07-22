d119b395128ba8dee33dede2e469645c662c651d
alvin <alvinchandra783@gmail.com>
2018-07-22 14:14:59 +0700

    tambah fitur encrypt

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -40,3 +42,24 @@ class ResetPasswordForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Request Password Reset')
+
+
+class EncryptForm(FlaskForm):
+    file = FileField('File to encrypt', validators=[FileRequired()])
+    base_key = PasswordField("Key", validators=[DataRequired()])
+    recipient = SelectField("Recipient", coerce=int)
+    comment = TextAreaField("Comment", validators=[DataRequired()])
+    password = PasswordField("Your password", validators=[Length(min=0, max=140)])
+    submit = SubmitField('Encrypt')
+
+    def __init__(self, *args, **kwargs):
+        super(EncryptForm, self).__init__(*args, **kwargs)
+        # self.sender = user
+        # tangani recipient
+        self.recipient.choices = [(current_user.id, current_user.username)]
+        self.recipient.choices += [(u.id, u.username) for u in User.query.filter(User.id != current_user.id).all()]
+        # self.recipient.choices.insert(0, '---Select User---')
+
+    def validate_password(self, password):
+        if current_user.check_password(password.data) is False:
+            raise ValidationError('Invalid password.')
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,18 @@ from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm, EncryptForm
+from app.models import User, Cipherfile, Message
 from app.email import send_password_reset_email, send_confirmation_link_email
 from app.decorators import check_confirmed
 
+import os
+import hashlib
+from kripto_core.pbkdf2 import pbkdf2
+from kripto_core.rsa import rsa_cipher
+from kripto_core.salsa20 import Salsa20
 
 @app.before_request
 def before_request():
@@ -51,8 +57,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+
         if user.confirmed is False:
             return redirect(url_for('unconfirmed'))
+
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -72,6 +80,7 @@ def confirm_email(token):
     if not user:
         return redirect(url_for('index'))
     user.confirmed = True
+    user.confirmed_timestamp = datetime.utcnow()
     db.session.add(user)
     db.session.commit()
     flash('You have confirmed your account.')
@@ -90,9 +99,53 @@ def user(username):
 @login_required
 @check_confirmed
 def encrypt():
-    if not current_user.confirmed:
-        pass
-    return render_template('encrypt.html', title='Encrypt')
+    form = EncryptForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        base_key = form.base_key.data
+        recipient = User.query.get(form.recipient.data) # instance objek user
+        data = file.read()  # data dlm bytes (stream)
+        comment = form.comment.data  # comment (pesan) untuk message
+
+        # digital signature digest
+        digest = hashlib.sha256(data).digest()
+        signed_digest = rsa_cipher.digital_signature(current_user.get_private_key(), digest)
+
+        # enkripsi kunci Salsa20
+        s20_key = pbkdf2(base_key)
+        enc_s20_key = rsa_cipher.encrypt(recipient.get_public_key(), s20_key)
+
+        # enkripsi data dari file
+        nonce = os.urandom(8)
+        s20 = Salsa20(s20_key, nonce)
+        enc_data = s20.encrypt(data)
+        enc_data = nonce + enc_data
+
+        # buat instance object cipherfile
+        cipherfile = Cipherfile(
+            filename=secure_filename(file.filename),
+            file_type=file.content_type,
+            file_length=len(data),
+            content=enc_data, # data yang telah dienkripsi dlm bytes <--
+            encrypted_s20_key=enc_s20_key,
+            signed_digest=signed_digest
+        )
+        db.session.add(cipherfile)
+
+        # buat instance object message
+        message = Message(
+            sender=current_user,
+            recipient=recipient,
+            cipherfile=cipherfile,
+            comment=comment,
+        )
+        db.session.add(message)
+        db.session.commit()
+
+        flash('File {} has been sucessfully encrypted.'.format(cipherfile.filename))    # success
+        # return '{} {} {}'.format(filename, recipient.username, len(data))
+        return redirect(url_for('outbox'))
+    return render_template('encrypt.html', title='Encrypt', form=form)
 
 
 @app.route('/about')
