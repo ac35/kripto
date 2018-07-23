7f248193085caf904233863e12e00c6134bb11d8
alvin <alvinchandra783@gmail.com>
2018-07-23 17:42:50 +0700

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -37,6 +38,15 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Kripto startup')
 
     return app
 
